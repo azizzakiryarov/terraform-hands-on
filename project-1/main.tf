e5f53063673df1abdd7742132b42d668d5b6f66b
Aziz Zakiryarov <azizzakiryarov@MBPsomtlhorAziz.lan>
2022-12-23 17:01:22 +0100

    Get started with dynamic

--- project-1/main.tf
+++ project-1/main.tf
@@ -65,67 +65,20 @@ resource "aws_security_group" "terraform-ubuntu-sg" {
     
     name = "terraform-ubuntu-sg"
     vpc_id = aws_vpc.terraform-ubuntu-vpc.id
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+    
+    dynamic "ingress" {
+      for_each = var.rules
+      content {
+        from_port = ingress.value["port"]
+        to_port = ingress.value["port"]
+        protocol = ingress.value["protocol"]
+        cidr_blocks = ingress.value["cidr_blocks"]
+      }
     }
-
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        from_port = 8443
-        to_port = 8443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    #egress {
-    #    from_port = 0
-    #    to_port = 0
-    #    protocol = "-1"
-    #    cidr_blocks = ["0.0.0.0/0"]
-    #}
-
     egress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 8443
-        to_port = 8443
-        protocol = "tcp"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
---
+++ project-1/variables.tf
@@ -0,0 +1,29 @@
+variable "rules" {
+    type = list(object({
+        port = number
+        protocol = string
+        cidr_blocks = list(string)
+    }))
+    default = [ 
+        {
+            port = 80
+            protocol = "tcp"
+            cidr_blocks = [ "0.0.0.0/0" ]
+        },
+        {
+            port = 22
+            protocol = "tcp"
+            cidr_blocks = [ "0.0.0.0/0" ]
+        },
+        {
+            port = 443
+            protocol = "tcp"
+            cidr_blocks = [ "0.0.0.0/0" ]
+        },
+        {
+            port = 8443
+            protocol = "tcp"
+            cidr_blocks = [ "0.0.0.0/0" ]
+        }
+    ]
+}
